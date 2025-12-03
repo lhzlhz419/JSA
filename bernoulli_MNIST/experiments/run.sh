33f0b571cf5a888591df80c687228fbde29b0bae
KeLi <ke-li24@mail.tsinghua.edu.cn>
2025-12-03 20:54:07 +0800

    Remove old files, remain only the latest ones.

--- bernoulli_MNIST/experiments/run.sh
+++
@@ -1,6 +0,0 @@
-net=$1
-seed=$2
-method=$3
-
-python run.py --method $method --net $net --seed $seed
-
--- categorical_MNIST/experiments/run.sh
+++
@@ -1,6 +0,0 @@
-seed=$1
-
-python jsa.py --seed $seed
-python vimco.py --seed $seed
-python gumbel.py --seed $seed
-python arsm.py --seed $seed
